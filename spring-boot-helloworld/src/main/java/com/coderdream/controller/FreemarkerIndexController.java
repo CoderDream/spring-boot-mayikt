2d6730347fc937ea1bc2c1819f82e1802d80aa5d
CoderDream <coderdream@gmail.com>
2021-06-03 11:38:55 +0800

    模板文件中加入比较逻辑、列表

--- spring-boot-helloworld/src/main/java/com/coderdream/controller/FreemarkerIndexController.java
+++ spring-boot-helloworld/src/main/java/com/coderdream/controller/FreemarkerIndexController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -24,11 +26,20 @@ public class FreemarkerIndexController {
     }
 
     @RequestMapping("/freemarkerIndex")
-    public String freemarkerIndex(Map<String, String> result, HttpServletRequest request) {
+    public String freemarkerIndex(Map<String, Object> result, HttpServletRequest request) {
         // 转发到页面展示数据name value mayikt
         FreeMarkerAutoConfiguration a = null;
         result.put("name", "mayikt");
         // request.setAttribute("name", "mayikt");
+
+        // 0为男，1为女
+        result.put("sex", "0");
+        result.put("age", 22);
+        List<String> userList = new ArrayList<>();
+        userList.add("CoderDream");
+        userList.add("xiaowei");
+        result.put("userList", userList);
+
         return "freemarkerIndex";
     }
 }
--- spring-boot-helloworld/src/main/resources/templates/freemarkerIndex.ftl
+++ spring-boot-helloworld/src/main/resources/templates/freemarkerIndex.ftl
@@ -6,5 +6,22 @@
 </head>
 <body>
 ${name}
+
+<#if sex=='0'>
+    男
+<#elseif sex=='1'>
+    女
+</#if>
+
+<#if age gt 17>
+    已经成年啦
+<#else>
+    未成年
+</#if>
+
+<#list userList as user>
+    ${user}
+</#list>
+
 </body>
 </html>
