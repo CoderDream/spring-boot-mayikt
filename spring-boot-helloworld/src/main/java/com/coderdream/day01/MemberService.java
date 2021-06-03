f2f5beafec020fbf006b7ce174ac00a0fb6d9c6f
CoderDream <coderdream@gmail.com>
2021-06-03 10:21:54 +0800

    SpringBoot支持两种配置方式，一种是properties文件，一种是yml。
    使用yml可以减少配置文件的重复性。

--- spring-boot-helloworld/src/main/java/com/coderdream/day01/MemberService.java
+++ spring-boot-helloworld/src/main/java/com/coderdream/day01/MemberService.java
@@ -1,10 +1,11 @@
 package com.coderdream.day01;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author ：yujl
+ * @author ：xul
  * @date ：Created in 2021/6/3 8:53
  * @description：
  * @modified By：
@@ -13,9 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberService {
 
+@Value("${mayikt.name}")
+private String name;
+
+@Value("${mayikt.age}")
+private String age;
+
+@RequestMapping("/getProperties")
+public String getProperties(){
+    return name + "--" + age;
+}
+
     @RequestMapping("/getMember")
     public String getMember(){
         return "CoderDream";
     }
-
 }
